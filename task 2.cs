0153944ff94ef287cadcdb778cf049c79039d3e5
yulianaromaniv <108033373+yulianaromaniv@users.noreply.github.com>
2022-07-11 12:51:30 +0300

    Add files via upload

---
+++ task 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string str;
+            int age;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("How old are you" + name + "?");
+            str = Console.ReadLine();
+            age = Convert.ToInt32(str);
+            Console.WriteLine("My name is " + name + ".My age is " + age);
+        }
+    }
+}
---
+++ task_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int a;
+            Console.WriteLine("Input value a:");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            int area = a * a;
+            int perimetr = 4 * a;
+            Console.WriteLine("Perimetr= " + perimetr);
+            Console.WriteLine("Area= " + area);
+        }
+    }
+}
